b08fcec616fd9f09ce0a1bc26e75cdf35cdad6e4
MohammadZahid000 <141526017+MohammadZahid000@users.noreply.github.com>
2024-11-27 11:53:21 +0530

    Add files via upload

---
+++ src/Components/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <div className='footer d-flex justify-content-center bg-dark'>
+    <h4>&copy; created by Zahid :All copy right are reserved</h4>
+    </div>
+  )
+}
